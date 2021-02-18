b6c3b3eafe371b90e1e5874d6f5665a5ec00aa81
Johnathan Wilson <johnathan@kellerson.co.uk>
2021-02-18 12:05:41 +0000

    Implementing a currentUser endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,6 +15,13 @@ const filterObj = (obj, ...allowedFields) => {
 // ADMIN USER CONTROLLERS
 ////////////////////////////////////////////////
 
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined! Please use /signup instead',
+  });
+};
+
 exports.getAllUsers = factory.getAll(User);
 exports.getUser = factory.getOne(User);
 exports.updateUser = factory.updateOne(User); // Do NOT update password with updateUser - use updatePassword from authController
@@ -23,6 +30,11 @@ exports.deleteUser = factory.deleteOne(User);
 /////////////////////////////////////////////////
 // LOGGED IN USER CONTROLLERS
 ////////////////////////////////////////////////
+
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
 
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
@@ -58,10 +70,3 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
     data: null,
   });
 });
-
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined! Please use /signup instead',
-  });
-};
