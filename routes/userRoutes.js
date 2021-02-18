47873ef281c6012e390216dd42e9ce651c2334f2
Johnathan Wilson <johnathan@kellerson.co.uk>
2021-02-18 12:25:50 +0000

    Updating the authentication and the Authorisations on the routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,24 +6,20 @@ const authController = require('../controllers/authController');
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
+// Protect All Routes after this point!
+router.use(authController.protect);
 
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.patch('/updateMyPassword', authController.updatePassword);
+
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+// Restrict All Routes after this point to ADMIN!
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
