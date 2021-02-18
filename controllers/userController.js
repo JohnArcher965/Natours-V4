57ca43dfd94cea67e9f13dfaf0f9c9d7c109645c
Johnathan Wilson <johnathan@kellerson.co.uk>
2021-02-18 11:30:47 +0000

    Implementing factory functions for update and create.

--- controllers/userController.js
+++ controllers/userController.js
@@ -73,11 +73,6 @@ exports.createUser = (req, res) => {
   });
 };
 
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
-  });
-};
-
+// Do NOT update password with updateUser
+exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
